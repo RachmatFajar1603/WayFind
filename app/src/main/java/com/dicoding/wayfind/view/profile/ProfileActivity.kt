d67838181714ea7e4f487d64ee8a83f635d0eb57
Rachmat Fajar <rachmat3@mhs.usk.ac.id>
2024-06-11 23:52:35 +0700

    update

--- app/src/main/java/com/dicoding/wayfind/view/profile/ProfileActivity.kt
+++ app/src/main/java/com/dicoding/wayfind/view/profile/ProfileActivity.kt
@@ -1,21 +1,80 @@
 package com.dicoding.wayfind.view.profile
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.dicoding.wayfind.R
+import com.dicoding.wayfind.view.favorite.FavoriteActivity
+import com.dicoding.wayfind.view.home.HomeActivity
+import com.dicoding.wayfind.view.map.TurnByTurnExperienceActivity
+import nl.joery.animatedbottombar.AnimatedBottomBar
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setupView()
+
+        val backButton = findViewById<ImageButton>(R.id.back_button_profile)
+        backButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()  // Optional: if you want to remove this activity from the stack
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
         }
+
+        val bottomBar = findViewById<AnimatedBottomBar>(R.id.bottom_bar)
+
+        bottomBar.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener {
+            override fun onTabSelected(
+                lastIndex: Int,
+                lastTab: AnimatedBottomBar.Tab?,
+                newIndex: Int,
+                newTab: AnimatedBottomBar.Tab
+            ) {
+                if (newTab.id == R.id.tab_maps) {
+                    val intent = Intent(this@ProfileActivity, TurnByTurnExperienceActivity::class.java)
+                    startActivity(intent)
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                } else if (newTab.id == R.id.tab_profile) {
+                    val intent = Intent(this@ProfileActivity, ProfileActivity::class.java)
+                    startActivity(intent)
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                } else if (newTab.id == R.id.tab_home) {
+                    val intent = Intent(this@ProfileActivity, HomeActivity::class.java)
+                    startActivity(intent)
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                } else if (newTab.id == R.id.tab_favorite) {
+                    val intent = Intent(this@ProfileActivity, FavoriteActivity::class.java)
+                    startActivity(intent)
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                }
+            }
+
+            override fun onTabReselected(index: Int, tab: AnimatedBottomBar.Tab) {
+                // Handle tab reselection if needed
+            }
+        })
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
